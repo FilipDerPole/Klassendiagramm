f4a2457253a2bb5b2486d3d577055f7c74fb6af6
FilipDerPole <45363739+FilipDerPole@users.noreply.github.com>
2018-12-10 16:31:58 +0100

    Add files via upload

---
+++ 3_OOP_UML_Rozana_Pfrendl/src/Schule.java
@@ -0,0 +1,46 @@
+
+public class Schule {
+
+	private String name;
+	private long schulkennzahl;
+	private long schultyp;
+
+	public:
+	
+	boolean addPersonal (NichtLehrerpersonal personal) {
+		
+	}
+
+	Abteilung addAbteilung(String name, String kuerzel) {
+
+	}
+
+	boolean setDirektor(Lehrer lehrer) {
+
+	}
+
+	long getSchulkennzahl() {
+
+	}
+
+	String getName() {
+
+	}
+
+	String getSchultyp() {
+
+	}
+
+	getPersonal() {
+		
+	}
+	
+	getSchueler() {
+		
+	}
+	
+	int getAnzahlSchueler() {
+		
+	}
+
+}
